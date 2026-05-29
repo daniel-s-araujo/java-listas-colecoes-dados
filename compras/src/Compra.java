dbb4627274554b05135294040c6142a55ef5724c
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-05-29 19:54:55 -0300

    Implementando o menu e ordenando dados

--- compras/src/Compra.java
+++ compras/src/Compra.java
@@ -1,4 +1,4 @@
-public class Compra {
+public class Compra implements Comparable<Compra> {
     private String descricao;
     private double valor;
 
@@ -18,5 +18,10 @@ public class Compra {
     @Override
     public String toString() {
         return "Compra: " + "descricao = " + descricao + " valor = " + valor;
+    }
+
+    @Override
+    public int compareTo(Compra outraCompra) {
+        return Double.valueOf(this.valor).compareTo(outraCompra.valor);
     }
 }
