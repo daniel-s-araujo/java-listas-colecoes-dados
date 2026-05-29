4d396605dd0c8b4eb1a9d5a58d1f8c0d8c7aca98
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-05-28 21:57:44 -0300

    Utilizando a interface List

--- screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -7,6 +7,7 @@ import br.com.alura.screenmatch.modelos.Titulo;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -20,7 +21,7 @@ public class PrincipalComListas {
 
         Filme f1 = filmeDoPaulo;
 
-        ArrayList<Titulo> lista = new ArrayList<>();
+        List<Titulo> lista = new ArrayList<>();
         lista.add(filmeDoPaulo);
         lista.add(outroFilme);
         lista.add(meuFilme);
@@ -33,7 +34,7 @@ public class PrincipalComListas {
             }
         }
 
-        ArrayList<String> buscaPorArtista = new ArrayList<>();
+        List<String> buscaPorArtista = new ArrayList<>();
         buscaPorArtista.add("Adam Sandler");
         buscaPorArtista.add("Paulo");
         buscaPorArtista.add("Jacqueline");
